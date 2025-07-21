606d16b893ac92592a36d158b0486bd321fb3f59
José Isaac <joseisaacnascimento@gmail.com>
2025-07-21 14:18:03 -0300

    feat: Cria as pastas das rotas de CadastroVenda e Vendas - Atualiza o front da página NotFound

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -3,8 +3,10 @@ import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import CadastroVeiculo from "./Pages/CadastroVeiculo";
 import CadastroCliente from "./Pages/CadastroCliente";
+// import CadastroVenda from "./Pages/CadastroVenda";
 import ListarVeiculos from "./Pages/ListarVeiculos";
 import ListarClientes from "./Pages/ListarClientes";
+// import ListarVendas from "./Pages/ListarVendas";
 import MainLayout from "./Components/MainLayout"; // novo layout com o Header
 
 const AllRoutes = () => {
@@ -16,8 +18,10 @@ const AllRoutes = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cadastro-veiculo" element={<CadastroVeiculo />} />
           <Route path="/cadastro-cliente" element={<CadastroCliente />} />
+          {/* <Route path="/cadastro-venda" element={<CadastroVenda />} /> */}
           <Route path="/veiculos" element={<ListarVeiculos />} />
           <Route path="/clientes" element={<ListarClientes />} />
+          {/* <Route path="/vendas" element={<ListarVendas />} /> */}
         </Route>
 
         {/* Página 404 fora do layout (opcional) */}
--- client/src/Pages/NotFound/index.jsx
+++ client/src/Pages/NotFound/index.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <section className="w-[90%] max-w-[800px] flex flex-col gap-4 justify-center items-center p-4 shadow-2xl rounded-2xl">
-        <h1 className="text-6xl font-bold ">Erro 404</h1>
-        <p className="text-2xl ">Página não encontrada</p>
-        <Link className="text-2xl font-bold hover:underline" to="/">
+    <div>
+      <section className="flex flex-col gap-2 justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Ops, página não encontrada!</h1>
+        <Link
+          className="text-2xl font-bold underline hover:text-blue-600 transition-all duration-75"
+          to="/"
+        >
           Voltar pro início
         </Link>
       </section>
