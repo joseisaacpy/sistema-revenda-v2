98de4d70182f7eec133bba3b171007b41940aa8f
José Isaac <joseisaacnascimento@gmail.com>
2025-07-28 19:22:53 -0300

    feat: Adiciona footer e configura main do MainLayout pra display flex e assim empurrar o footer pro fim da página

---
+++ client/src/Components/Footer/index.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-900 text-white p-2 flex items-center justify-center">
+      <p>
+        Desenvolvido por{" "}
+        <a
+          href="https://github.com/joseisaacpy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-yellow-400 transition-all duration-300"
+        >
+          José Isaac
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/Components/MainLayout/index.jsx
+++ client/src/Components/MainLayout/index.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import Footer from "../Footer";
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="flex flex-col min-h-screen">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
