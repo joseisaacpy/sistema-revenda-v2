cbba3eda1b02fbe5b2927e79e2d7ee26b0ecfa08
José Isaac <joseisaacnascimento@gmail.com>
2025-08-06 21:41:57 -0300

    chore: Comenta logs do middleware de autenticação

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -6,6 +6,10 @@ const SECRET_KEY = process.env.SECRET_KEY;
 export function verificarToken(req, res, next) {
   const authHeader = req.headers.authorization;
 
+  // Logs informativos
+  // console.log("Todos os headers recebidos:", req.headers); // <-- Adicione isso
+  // console.log("Authorization header recebido:", authHeader);
+
   if (!authHeader) {
     return res.status(401).json({ msg: "Token não encontrado." });
   }
@@ -14,9 +18,11 @@ export function verificarToken(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, SECRET_KEY);
+    // console.log("Token válido:", decoded);
     req.usuario = decoded;
     next();
   } catch (error) {
+    console.error("Token inválido:", error.message);
     res.status(403).json({ msg: "Token inválido." });
   }
 }
